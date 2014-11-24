3b1216d77bd671353d49c55a1ecf1d9f2ee9e7f9
diszell2008 <listening2009@gmail.com>
2014-11-25 01:25:17 +0700

    Fix Error Liblights.c

--- liblight/lights.c
+++ liblight/lights.c
@@ -275,8 +275,6 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_notifications;
     else if (0 == strcmp(LIGHT_ID_BATTERY, name))
         set_light = set_light_battery;
-    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
-        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
     else
