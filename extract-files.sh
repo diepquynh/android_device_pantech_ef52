21385f2830876f1e8d1f83da2bb303ef92195d39
diszell2008 <listening2009@gmail.com>
2014-11-24 18:46:15 +0700

    Edit proprietary-blobs.txt

--- extract-files.sh
+++ extract-files.sh
@@ -11,7 +11,7 @@ for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ `; do
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
-    cp ~/good-rom/system/$FILE $BASE/$FILE
+ 	adb pull /system/$FILE $BASE/$FILE
 done
 
 ./setup-makefiles.sh
