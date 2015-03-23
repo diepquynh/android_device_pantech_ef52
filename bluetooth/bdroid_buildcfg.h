5efe70e6b2e0ffdb7fed3e072da8f4974ca9c1e8
futranbg <nhoc_lau_ca@yahoo.com>
2015-03-23 13:57:17 +0700

    Fix Bluetooth Speaker Phone Function

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,14 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "VEGA Iron"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BLUETOOTH_QTI_SW TRUE
 
+#define BTM_DEF_LOCAL_NAME   "VEGA R3"
+// Enables Interleave scan
+#define BTA_HOST_INTERLEAVE_SEARCH  TRUE
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define MAX_L2CAP_CHANNELS    14
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  TRUE
 #endif
